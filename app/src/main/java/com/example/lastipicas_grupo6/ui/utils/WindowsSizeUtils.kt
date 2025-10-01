7544de9c3248b8569255ce0d32199d9ee59fa930
ReXx1312 <alex.ignaciorz@gmail.com>
2025-09-30 21:00:59 -0300

    probando2

---
+++ app/src/main/java/com/example/lastipicas_grupo6/ui/utils/WindowsSizeUtils.kt
@@ -0,0 +1,14 @@
+package com.example.lastipicas_grupo6.ui.utils
+
+import androidx.activity.compose.LocalActivity
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
+
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Composable
+fun obtenerWindowSizeclaa(): WindowSizeClass {
+    return calculateWindowSizeClass(LocalActivity.current as android.app.Activity)
+}
